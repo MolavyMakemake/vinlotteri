b8d12671d09b5f85d40327c2d21e2115dc247cdd
MolavyMakemake <molavy.business@gmail.com>
2024-10-17 23:20:01 +0200

    .

---
+++ contestant.gd
@@ -0,0 +1,41 @@
+class_name Contestant
+	
+var instance: Node2D
+var animation: AnimationPlayer
+
+var position: int = 0
+var timeout: float = -1
+
+var active: bool = false
+
+func initiate(_instance: Node2D):
+	instance = _instance
+	animation = _instance.get_node("AnimationPlayer")
+
+	position = 0
+	timeout = -1
+
+func ready():
+	active = true
+	animation.play("run")
+
+func jump(_success: bool):
+	if _success:
+		animation.play("jump")
+	else:
+		timeout = 1
+		position += 1
+		animation.play("fall")
+		
+	animation.queue("run")
+	
+func tick(_delta: float, _move: bool):
+	timeout -= _delta
+	
+	if active:
+		position += int(timeout <= 0 and _move)
+		instance.position.x = position
+
+func finish():
+	active = false
+	animation.play("finish")
--- gamelogic.gd
+++ gamelogic.gd
@@ -1,60 +1,96 @@
 extends Node2D
+
 @onready var resource_preloader = $ResourcePreloader
 @onready var camera = $Camera2D
 
-@export var track_length: int = 400
 @export var n_fences: int = 10
 @export var move_probability : Curve
+@export var jump_probability : Curve
 @export var tracks : Array
+
+@export var track_layout: Array
 
 @export var keys : Array = ["default", "mario"]
 
-var position_v = []
-var competitor_v = []
 var n_competitors = 0
+var contestants_v = []
 
-var pos_fence = 0
+var track_length = 0
+var max_pos = 0
+
+var rng = RandomNumberGenerator.new()
+var is_waiting = true
 
 func _ready() -> void:
-	randomize()
-	pos_fence = track_length / n_fences
+	rng.randomize()
+	print(rng.seed)
 	
-	for i in range(n_fences):
-		for y in tracks:
-			var instance = resource_preloader.get_resource("fence").instantiate()
-			add_child(instance)
-			instance.position = Vector2(i * pos_fence, y)
 	
-	n_competitors = len(keys)
+	track_length = 0
+	for k in range(0, len(track_layout), 2):
+		var fence_pos = 0
+		if track_layout[k+1] > 0:
+			fence_pos = track_layout[k] / track_layout[k+1]
+			
+			for i in range(0, track_layout[k], fence_pos):
+				for y in tracks:
+					var instance = resource_preloader.get_resource("fence").instantiate()
+					add_child(instance)
+					instance.position = Vector2(track_length + i, y)
+				
+		track_layout[k+1] = fence_pos
+		track_length += track_layout[k]
+	
+	$track/finish.position.x = track_length
+	
+	n_competitors = min(8, len(keys))
 	for i in range(n_competitors):
 		var instance = resource_preloader.get_resource(keys[i]).instantiate()
+		instance.position.y = tracks[i]
 		add_child(instance)
 		
-		instance.position.y = tracks[i]
-		instance.get_node("AnimationPlayer").play("run")
-		
-		competitor_v.push_back(instance)
-		position_v.push_back(0)
+		var contestant = Contestant.new()
+		contestant.initiate(instance)
+		contestants_v.push_back(contestant)
+
+func is_fence(p: int):
+	for i in range(0, len(track_layout), 2):
+		if p < track_layout[i]:
+			return track_layout[i+1] != 0 and p % track_layout[i+1] == 0
+			
+		p -= track_layout[i]
+	
+	return false
 
 func _physics_process(delta: float) -> void:
+	if is_waiting:
+		return
 	
-	var mean_pos = 0
-	var max_pos = 0
+	var mean_pos = 0.0
+	var _pos_offset = 1 - 0.01 * max_pos
 	for i in range(n_competitors):
-		var pos = position_v[i] + int(randf() < 0.5)
+		var con = contestants_v[i]
+		var probability_weight = max(0, _pos_offset + 0.01 * con.position)
+		var move = rng.randf() < move_probability.sample(probability_weight)
 		
-		if (pos % pos_fence == 0):
-			print("fence!")
-			
-		if (pos >= track_length):
-			print("finished")
+		if (is_fence(con.position)):
+			con.jump(rng.randf() < jump_probability.sample(probability_weight))
+			move = true
 		
-		position_v[i] = pos
-		competitor_v[i].position.x = pos
+		con.tick(delta, move)
+		if (con.active and con.position >= track_length):
+			con.finish()
+			print("Nr. ", i+1, " ", keys[i], " has finished!")
 		
-		mean_pos += pos
-		max_pos = max(pos, max_pos)
+		mean_pos += con.position
+		max_pos = max(con.position, max_pos)
 		
-	var d_pos = mean_pos / n_competitors - camera.position.x
-	camera.position.x += d_pos * delta
+	mean_pos /= n_competitors
+	var target = mean_pos + 60 * (1 - 2.0 * mean_pos / track_length)
+	camera.position.x += (target - camera.position.x) * delta
 	
+func _input(event):
+	if is_waiting and event.is_action_pressed("begin"):
+		is_waiting = false
+		for con in contestants_v:
+			con.ready()
